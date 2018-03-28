03e1708a68e23f202bbdee60f93aeb75e57fb0a6
Jenny Bryan <jenny.f.bryan@gmail.com>
2018-03-28 16:29:59 -0700

    glue-y wrappers around message(), warning(), stop()

---
+++ R/glue.R
@@ -0,0 +1,56 @@
+#' @importFrom glue glue glue_data collapse
+NULL
+
+stop_glue <- function(..., .sep = "", .envir = parent.frame(),
+                      call. = FALSE, .domain = NULL) {
+  stop(
+    glue(..., .sep = .sep, .envir = .envir),
+    call. = call., domain = .domain
+  )
+}
+
+stop_glue_data <- function(..., .sep = "", .envir = parent.frame(),
+                           call. = FALSE, .domain = NULL) {
+  stop(
+    glue_data(..., .sep = .sep, .envir = .envir),
+    call. = call., domain = .domain
+  )
+}
+
+stop_collapse <- function(x) stop(collapse(x, sep = "\n"), call. = FALSE)
+
+warning_glue <- function(..., .sep = "", .envir = parent.frame(),
+                         call. = FALSE, .domain = NULL) {
+  warning(
+    glue(..., .sep = .sep, .envir = .envir),
+    call. = call., domain = .domain
+  )
+}
+
+warning_glue_data <- function(..., .sep = "", .envir = parent.frame(),
+                              call. = FALSE, .domain = NULL) {
+  warning(
+    glue_data(..., .sep = .sep, .envir = .envir),
+    call. = call., domain = .domain
+  )
+}
+
+warning_collapse <- function(x) warning(collapse(x, sep = "\n"))
+
+message_glue <- function(..., .sep = "", .envir = parent.frame(),
+                         .domain = NULL, .appendLF = TRUE) {
+  message(
+    glue(..., .sep = .sep, .envir = .envir),
+    domain = .domain, appendLF = .appendLF
+  )
+}
+
+message_glue_data <- function(..., .sep = "", .envir = parent.frame(),
+                              .domain = NULL) {
+  message(
+    glue_data(..., .sep = .sep, .envir = .envir),
+    domain = .domain
+  )
+}
+
+message_collapse <- function(x) message(collapse(x, sep = "\n"))

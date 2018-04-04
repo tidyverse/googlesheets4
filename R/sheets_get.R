20956ba6e80cb089739908edd513bc5873ef1b6e
Jenny Bryan <jenny.f.bryan@gmail.com>
2018-04-04 13:09:40 -0700

    Start a file for cell range utils ... more are coming

--- R/sheets_get.R
+++ R/sheets_get.R
@@ -89,13 +89,3 @@ sheets_spreadsheet <- function(x = list()) {
 
   out
 }
-
-make_range <- function(start_row, end_row, start_column, end_column,
-                       sheet_name, ...) {
-  cl <- cellranger::cell_limits(
-    ul = c(start_row, start_column),
-    lr = c(end_row, end_column),
-    sheet = sq(sheet_name)
-  )
-  cellranger::as.range(cl, fo = "A1")
-}

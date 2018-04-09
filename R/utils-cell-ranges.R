a8ebf5d22db2a7280020303d27132234382fa781
Jenny Bryan <jenny.f.bryan@gmail.com>
2018-04-08 23:14:37 -0700

    Avoid modify_if()
    CRAN version does not preserve type of atomic vectors
    
    https://github.com/tidyverse/purrr/issues/417
    https://github.com/tidyverse/purrr/pull/463

--- R/utils-cell-ranges.R
+++ R/utils-cell-ranges.R
@@ -3,24 +3,18 @@
 sq_escape <- function(x) {
   ## if string already starts and ends with single quote, pass it through
   is_not_quoted <- !map_lgl(x, ~ grepl("^'.*'$", .x))
-  purrr::modify_if(
-    x,
-    is_not_quoted,
-    ## duplicate each single quote and protect string with single quotes
-    ~ paste0("'", gsub("'", "''", .x), "'")
-  )
+  ## duplicate each single quote and protect string with single quotes
+  x[is_not_quoted] <- paste0("'", gsub("'", "''", x[is_not_quoted]), "'")
+  x
 }
 
 sq_unescape <- function(x) {
   ## only modify if string starts and ends with single quote
   is_quoted <- map_lgl(x, ~ grepl("^'.*'$", .x))
-  purrr::modify_if(
-    x,
-    is_quoted,
-    ## strip leading and trailing single quote and substitute 1 single quote
-    ## for every pair of single quotes
-    ~ gsub("''", "'", sub("^'(.*)'$", "\\1", .x))
-  )
+  ## strip leading and trailing single quote and substitute 1 single quote
+  ## for every pair of single quotes
+  x[is_quoted] <- gsub("''", "'", sub("^'(.*)'$", "\\1", x[is_quoted]))
+  x
 }
 
 make_range <- function(start_row, end_row, start_column, end_column,

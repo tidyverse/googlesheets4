e2310e5d041967983908b8abefbb1b23098bb847
Jenny Bryan <jenny.f.bryan@gmail.com>
2019-05-08 12:17:50 -0700

    `gargle::is_legit_token()` is gone

--- R/sheets_auth.R
+++ R/sheets_auth.R
@@ -63,7 +63,7 @@ sheets_auth <- function(email = NULL,
     cache = cache,
     use_oob = use_oob
   )
-  if (!gargle::is_legit_token(cred, verbose = TRUE)) {
+  if (!inherits(cred, "Token2.0")) {
     stop(
       "Can't get Google credentials.\n",
       "Are you running googlesheets4 in a non-interactive session? Consider:\n",

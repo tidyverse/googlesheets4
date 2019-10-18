8e928c24d89cb15f15f9ff242dc615e8a7b96061
Jenny Bryan <jenny.f.bryan@gmail.com>
2019-10-17 21:51:05 -0700

    Put teeth in this test

--- tests/testthat/test-read_sheet.R
+++ tests/testthat/test-read_sheet.R
@@ -3,8 +3,9 @@ test_that("read_sheet() does same old, same old", {
   skip_if_offline()
   skip_if_no_token()
 
-  expect_known_output(
-    read_sheet(test_sheet("googlesheets4-cell-tests")),
+  expect_known_value(
+    # https://github.com/tidyverse/dplyr/issues/2751
+    as.data.frame(read_sheet(test_sheet("googlesheets4-cell-tests"))),
     ref("googlesheets4-cell-tests.rds")
   )
 })

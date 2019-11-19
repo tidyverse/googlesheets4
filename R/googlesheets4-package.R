05b1f2c6c288c56ba953289263e36159062a6b4a
Jenny Bryan <jenny.f.bryan@gmail.com>
2019-11-19 14:09:54 -0800

    Import transpose

--- R/googlesheets4-package.R
+++ R/googlesheets4-package.R
@@ -1,4 +1,4 @@
 #' @keywords internal
 #' @importFrom purrr %||% map_lgl map_int map_dbl map_chr map map2 pluck walk
-#'   pmap_chr set_names discard keep flatten modify_if compact
+#'   pmap_chr set_names discard keep flatten modify_if compact transpose
 "_PACKAGE"

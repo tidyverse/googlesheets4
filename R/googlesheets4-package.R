ebf1c86fc0765bd5c2bc68c93eff3ffe2bb4208a
Jenny Bryan <jenny.f.bryan@gmail.com>
2019-11-21 10:09:15 -0800

    Import imap

--- R/googlesheets4-package.R
+++ R/googlesheets4-package.R
@@ -1,4 +1,4 @@
 #' @keywords internal
 #' @importFrom purrr %||% map_lgl map_int map_dbl map_chr map map2 pluck walk
-#'   pmap_chr set_names discard keep flatten modify_if compact transpose
+#'   pmap_chr set_names discard keep flatten modify_if compact transpose imap
 "_PACKAGE"

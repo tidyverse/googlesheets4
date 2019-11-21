4121f39ba66ffbaeab9bf7c4bdc5a2264040e6ba
Jenny Bryan <jenny.f.bryan@gmail.com>
2019-11-20 17:02:28 -0800

    Make some "tidy" schemas available as internal data

--- data-raw/discovery-doc-prep.R
+++ data-raw/discovery-doc-prep.R
@@ -4,6 +4,10 @@ source(
   system.file("discovery-doc-ingest", "ingest-functions.R", package = "gargle")
 )
 
+# if my use of schemas works out well, maybe this will migrate upstream into
+# gargle and join the other ingest helpers
+source(here::here("data-raw", "schema-rectangling.R"))
+
 x <- download_discovery_document("sheets:v4")
 dd <- read_discovery_document(x)
 
@@ -23,4 +27,21 @@ attr(.endpoints, "base_url") <- dd$rootUrl
 # to the "flattened" or "inlined" representation currently in .endpoints
 .schemas <- pluck(dd, "schemas")
 
-usethis::use_data(.endpoints, .schemas, internal = TRUE, overwrite = TRUE)
+these <- c(
+  "Spreadsheet",
+  "SpreadsheetProperties",
+  "Sheet",
+  "SheetProperties",
+  "NamedRange",
+  "GridRange"
+)
+
+.tidy_schemas <- these %>%
+  set_names() %>%
+  map(schema_rectangle)
+# View(.tidy_schemas)
+
+usethis::use_data(
+  .endpoints, .schemas, .tidy_schemas,
+  internal = TRUE, overwrite = TRUE
+)

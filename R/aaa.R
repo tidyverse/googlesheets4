2960e3fcf03d3f0d2aa7f2cb50547ed3291407d0
Jenny Bryan <jenny.f.bryan@gmail.com>
2019-12-10 12:13:31 -0800

    I think pkgdown should use the docs account, not testing

--- R/aaa.R
+++ R/aaa.R
@@ -3,7 +3,7 @@
   if (identical(Sys.getenv("IN_PKGDOWN"), "true") &&
       gargle:::secret_can_decrypt("googlesheets4") &&
       !is.null(curl::nslookup("sheets.googleapis.com", error = FALSE))) {
-    sheets_auth_testing(drive = TRUE)
+    sheets_auth_docs(drive = TRUE)
   }
 
   invisible()

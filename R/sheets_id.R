a85ee401e8247df856e23e6f7690c1c9035205d3
Jenny Bryan <jenny.f.bryan@gmail.com>
2019-12-10 16:26:08 -0800

    Move this into existing file; fix example

--- R/sheets_id.R
+++ R/sheets_id.R
@@ -158,3 +158,22 @@ one_id <- function(x) {
     gsub("/d/|/folders/|id=", "", regmatches(x, id_loc))
   }
 }
+
+#' Extract the file id from Sheet metadata
+#'
+#' This method implements [googledrive::as_id()] for the class used here to hold
+#' metadata for a Sheet. It just calls [as_sheets_id()], but it's handy in case
+#' you forget that exists and hope that `as_id()` will "just work".
+#'
+#' @inheritParams googledrive::as_id
+#' @param x An instance of `sheets_Spreadsheet`, which is returned by, e.g.,
+#'   [sheets_get()].
+#' @inherit googledrive::as_id return
+#' @export
+#' @examples
+#' if (sheets_has_token()) {
+#'   ss <- sheets_get(sheets_example("mini-gap"))
+#'   class(ss)
+#'   as_id(ss)
+#' }
+as_id.sheets_Spreadsheet <- function(x, ...) as_sheets_id(x)

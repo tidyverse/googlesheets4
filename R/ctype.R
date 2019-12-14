ec6f03fd0b1f7affec7792105d7e223d930b8854
Jenny Bryan <jenny.f.bryan@gmail.com>
2019-12-14 14:05:50 -0800

    Make one, bigger switch()

--- R/ctype.R
+++ R/ctype.R
@@ -142,10 +142,10 @@ apply_ctype <- function(cell_list, na = "", trim_ws = TRUE) {
 }
 
 infer_ctype <- function(cell, na = "", trim_ws = TRUE) {
-  ## Blank cell criteria
-  ##   * cell is NULL or list()
-  ##   * cell has no effectiveValue
-  ##   * formattedValue matches an `na` string
+  # Blank cell criteria
+  #   * cell is NULL or list()
+  #   * cell has no effectiveValue
+  #   * formattedValue matches an `na` string
   if ( length(cell) == 0 ||
        length(cell[["effectiveValue"]]) == 0 ||
        is_na_string(cell[["formattedValue"]], na = na, trim_ws = trim_ws)
@@ -155,24 +155,20 @@ infer_ctype <- function(cell, na = "", trim_ws = TRUE) {
 
   effective_type <- .extended_value[[names(cell[["effectiveValue"]])]]
 
-  if (effective_type == "error") {
-    return("CELL_BLANK")
-  }
-
-  if (effective_type == "formula") {
-    warning_glue("Cell has formula as effectiveValue. I thought impossible!")
-    return("CELL_TEXT")
-  }
-
-  if (effective_type != "number") {
+  if (!identical(effective_type, "number")) {
     return(switch(
       effective_type,
-      string = "CELL_TEXT",
+      error   = "CELL_BLANK",
+      string  = "CELL_TEXT",
       boolean = "CELL_LOGICAL",
+      formula = {
+        warning_glue("Cell has formula as effectiveValue. I thought impossible!")
+        "CELL_TEXT"
+      },
       stop_glue("Unhandled effective_type: {sq(effective_type)}")
     ))
   }
-  ## only numeric cells remain
+  # only numeric cells remain
 
   nf_type <- pluck(
     cell,

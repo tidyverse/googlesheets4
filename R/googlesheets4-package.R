007af965df561cdd9b753a7999b3c9a47444a8f8
Jenny Bryan <jenny.f.bryan@gmail.com>
2019-12-24 22:09:50 -0800

    Import the := operator

--- R/googlesheets4-package.R
+++ R/googlesheets4-package.R
@@ -2,5 +2,6 @@
 #' @importFrom purrr %||% map_lgl map_int map_dbl map_chr map map2 pluck walk
 #'   pmap pmap_chr set_names discard keep flatten modify modify_if
 #'   compact transpose imap
+#' @importFrom rlang :=
 #' @importFrom tibble as_tibble
 "_PACKAGE"

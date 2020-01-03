86e114a4a15e60ff9242f53df7ceaa2930fd2cfa
Jenny Bryan <jenny.f.bryan@gmail.com>
2020-01-03 00:14:12 -0800

    Move into own file

--- R/schema_Sheet.R
+++ R/schema_Sheet.R
@@ -38,10 +38,3 @@ as_Sheet.data.frame <- function(df, name) {
     )
   )
 }
-
-as_RowData <- function(df) {
-  df_cells <- modify(df, as_CellData)
-  df_rows <- pmap(df_cells, list)
-  df_rows <- c(list(as_CellData(names(df))), df_rows)
-  map(df_rows, ~ list(values = unname(.x)))
-}

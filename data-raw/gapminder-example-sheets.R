f67102919b90647c4f79001653484bf23ee23311
Jenny Bryan <jenny.f.bryan@gmail.com>
2020-02-07 12:11:36 -0800

    Back fill code to make gapminder and mini-gap example Sheets
    Update them to have styling that googlesheets4 applies when writing

---
+++ data-raw/gapminder-example-sheets.R
@@ -0,0 +1,42 @@
+library(tidyverse)
+library(googledrive)
+library(googlesheets4)
+library(gapminder)
+
+googlesheets4:::sheets_auth_docs()
+
+# if I were making the gapminder sheet from scratch, here's what I would do now:
+ss <- sheets_create(
+  "gapminder-reboot",
+  sheets = split(gapminder, gapminder$continent)
+)
+drive_trash(ss)
+# but I am not doing this -- I want to keep the existing Sheet ID
+# instead I will edit it (and mini-gap) in situ
+
+## Update gapminder example Sheet ----
+ss <- sheets_find("gapminder") %>% as_sheets_id()
+# sheets_browse(ss)
+
+gapminder_split <- split(gapminder, gapminder$continent)
+sheets_write(gapminder_split$Africa,   ss = ss, sheet = "Africa")
+sheets_write(gapminder_split$Americas, ss = ss, sheet = "Americas")
+sheets_write(gapminder_split$Asia,     ss = ss, sheet = "Asia")
+sheets_write(gapminder_split$Europe,   ss = ss, sheet = "Europe")
+sheets_write(gapminder_split$Oceania,  ss = ss, sheet = "Oceania")
+
+## Update mini-gap example Sheet ----
+mini_gap <- gapminder %>%
+  arrange(year) %>%
+  group_by(continent) %>%
+  slice(1:5) %>%
+  split(.$continent)
+
+(ss <- sheets_find("mini-gap") %>% as_sheets_id())
+# sheets_browse(ss)
+
+sheets_write(mini_gap$Africa,   ss = ss, sheet = "Africa")
+sheets_write(mini_gap$Americas, ss = ss, sheet = "Americas")
+sheets_write(mini_gap$Asia,     ss = ss, sheet = "Asia")
+sheets_write(mini_gap$Europe,   ss = ss, sheet = "Europe")
+sheets_write(mini_gap$Oceania,  ss = ss, sheet = "Oceania")

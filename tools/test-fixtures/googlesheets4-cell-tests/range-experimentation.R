88afe5ba94be1b27927f4b1c4a15fd9b71dbe314
Jenny Bryan <jenny.f.bryan@gmail.com>
2020-04-02 12:24:22 -0700

    Another place to use sheets_fodder()

--- tools/test-fixtures/googlesheets4-cell-tests/range-experimentation.R
+++ tools/test-fixtures/googlesheets4-cell-tests/range-experimentation.R
@@ -1,15 +1,10 @@
-library(tidyverse)
 devtools::load_all() # I assume we're in googlesheets4 source
-library(googledrive)
 
 googlesheets4:::sheets_auth_testing()
 
 ss <- test_sheet_create("googlesheets4-cell-tests")
 sheets_browse(ss)
 
-n <- 5
-df <- expand_grid(column = LETTERS[seq_len(n)], row = seq_len(n) + 1) %>%
-  unite("cell", sep = "", remove = FALSE) %>%
-  pivot_wider(id_cols = row, names_from = column, values_from = cell) %>%
-  select(-row)
+df <- sheets_fodder(5)
 sheets_write(df, ss, sheet = "range-experimentation")
+w

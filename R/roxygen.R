331d94a3f2591023e49fc4d4603e240b3091b598
Jenny Bryan <jenny.f.bryan@gmail.com>
2020-04-08 15:16:54 -0700

    Add missing paren

--- R/roxygen.R
+++ R/roxygen.R
@@ -6,7 +6,7 @@ param_ss <- function(..., pname = "ss") {
     @param {pname} \\
     Something that identifies a Google Sheet: its file ID, a URL from
     which we can recover the ID, an instance of `googlesheets4_spreadsheet`
-    (returned by [sheets_get()], or a [`dribble`][googledrive::dribble], which
+    (returned by [sheets_get()]), or a [`dribble`][googledrive::dribble], which
     is how googledrive represents Drive files. Processed through
     [as_sheets_id()].
     ")

0773f7672c3f0568d04a002693a0c61af14ae3cf
Jenny Bryan <jenny.f.bryan@gmail.com>
2020-04-08 19:09:26 -0700

    Upgrade the deprecation technology

--- R/deprecated.R
+++ R/deprecated.R
@@ -1,15 +1,26 @@
 #' Deprecated functions
 #'
+#' @description
+#' \lifecycle{deprecated}
+#'
+#' These functions are deprecated and will be removed in a future release of
+#' googlesheets4.
+#'
 #' @keywords internal
 #' @name googlesheets4-deprecated
+#' @importFrom lifecycle deprecate_warn
 NULL
 
-#' Use `sheets_sheet_names()` instead of `sheets_sheets()`.
+# nocov start
+
+#' @description
+#' `sheets_sheets()` is replaced by `sheets_sheet_names()`.
 #' @rdname googlesheets4-deprecated
 #' @inheritParams read_sheet
 #' @export
 sheets_sheets <- function(ss) {
-  .Deprecated("sheets_sheet_names()", package = "googlesheets4",
-              old = "sheets_sheets()")
+  deprecate_warn("0.2.0", "sheets_sheets()", "sheets_sheet_names()")
   sheets_sheet_names(ss)
 }
+
+# nocov end

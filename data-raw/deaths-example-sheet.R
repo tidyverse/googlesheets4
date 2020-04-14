c42060e7d8171896bf0fe6d7c8b36aab38ca5226
Jenny Bryan <jenny.f.bryan@gmail.com>
2020-04-14 16:37:12 -0700

    Re-create 'deaths' yet again
    I have to figure out how this is getting deleted!

--- data-raw/deaths-example-sheet.R
+++ data-raw/deaths-example-sheet.R
@@ -6,10 +6,26 @@ library(readxl)
 googlesheets4:::gs4_auth_docs()
 gs4_user()
 
-deaths_xlsx <- readxl_example("deaths.xlsx")
+x <- gs4_find()
 
+if ("deaths" %in% x$name) {
+  stop("Yo, 'deaths' already exists. Are you sure you want to do this?")
+}
+
+# to work on an existing 'deaths' Sheet
+# ss <- gs4_find("deaths") %>% as_sheets_id()
+
+# to create one from scratch
+deaths_xlsx <- readxl_example("deaths.xlsx")
 ss <- drive_upload(deaths_xlsx, name = "deaths", type = "spreadsheet")
 
-gs4_share(ss, type = "user", emailAddress = "jenny@rstudio.com", role = "writer")
+googlesheets4:::gs4_share(ss)
 
-# in the browser, I created named data ranges 'arts_data' and 'other_data'
+gs4_browse(ss)
+
+googlesheets4:::range_add_named(ss, name = "arts_data", range = "arts!A5:F15")
+googlesheets4:::range_add_named(ss, name = "other_data", range = "other!A5:F15")
+
+ss
+
+unclass(ss)

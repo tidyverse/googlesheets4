80ecf8fc79987eee64dfbe4527f4f7e3938468f4
Jenny Bryan <jenny.f.bryan@gmail.com>
2020-04-14 08:30:17 -0700

    Rationalizaing the deprecated functions and docs

--- R/deprecated.R
+++ R/deprecated.R
@@ -4,62 +4,35 @@
 #' \lifecycle{deprecated}
 #'
 #' These functions are deprecated and will be removed in a future release of
-#' googlesheets4.
+#' googlesheets4. Since googlesheets4 is a relatively young package, this
+#' removal will happen rapidly, possibly after just 1 release cycle.
 #'
 #' @param ... Passed on to the function that succeeds the deprecated function.
 #'
-#' @keywords internal
-#' @name googlesheets4-deprecated
-#' @importFrom lifecycle deprecate_warn
-NULL
-
-# nocov start
-
-#' @description
-#' `sheets_sheets()` is replaced by [sheet_names()].
-#' @rdname googlesheets4-deprecated
-#' @export
-sheets_sheets <- function(...) {
-  deprecate_warn("0.2.0", "sheets_sheets()", "sheet_names()")
-  sheet_names(...)
-}
-
-#' @description
-#' `sheets_cells()` is replaced by [range_read_cells()].
-#' @rdname googlesheets4-deprecated
-#' @export
-sheets_cells <- function(...) {
-  deprecate_warn("0.2.0", "sheets_cells()", "range_read_cells()")
-  range_read_cells(...)
-}
-
-#' @description
-#' `sheets_read()` is replaced by [range_read()] (which is a synonym for
-#' [read_sheet()]).
-#' @rdname googlesheets4-deprecated
-#' @export
-sheets_read <- function(...) {
-  deprecate_warn("0.2.0", "sheets_read()", "range_read()")
-  range_read(...)
-}
-
-#' @description
-#' `sheets_write()` is replaced by [sheet_write()] (which is a synonym for
-#' [write_sheet()]).
-#' @rdname googlesheets4-deprecated
-#' @export
-sheets_write <- function(...) {
-  deprecate_warn("0.2.0", "sheets_write()", "sheet_write()")
-  sheet_write(...)
-}
-
-#' @section Spreadsheet level operations:
+#' @section Auth and API endpoints:
+#'
+#' ```{r echo = FALSE}
+#' dat <- tibble::tribble(
+#'                ~ "< v0.2.0", ~ ">= v0.2.0",
+#'          "sheets_api_key()",   "gs4_api_key()",
+#'             "sheets_auth()",   "gs4_auth()",
+#'   "sheets_auth_configure()",   "gs4_auth_configure()",
+#'           "sheets_deauth()",   "gs4_deauth()",
+#'        "sheets_endpoints()",   "gs4_endpoints()",
+#'        "sheets_has_token()",   "gs4_has_token()",
+#'        "sheets_oauth_app()",   "gs4_oauth_app()",
+#'            "sheets_token()",   "gs4_token()",
+#'             "sheets_user()",   "gs4_user()"
+#' )
+#' knitr::kable(dat, col.names = paste0("**", colnames(dat), "**"))
+#' ```
+#'
+#' @section Spreadsheet operations:
 #'
 #' ```{r echo = FALSE}
 #' dat <- tibble::tribble(
 #'          ~ "< v0.2.0", ~ ">= v0.2.0",
 #'     "sheets_browse()",   "gs4_browse()",
-#'     "sheets_create()",   "gs4_create()",
 #'       "sheets_find()",   "gs4_find()",
 #'    "sheets_example()",   "gs4_example()",
 #'   "sheets_examples()",   "gs4_examples()",
@@ -68,18 +41,53 @@ sheets_write <- function(...) {
 #' knitr::kable(dat, col.names = paste0("**", colnames(dat), "**"))
 #' ```
 #'
+#' @section (Work)sheet operations:
+#'
+#' ```{r echo = FALSE}
+#' dat <- tibble::tribble(
+#'        ~ "< v0.2.0", ~ ">= v0.2.0",
+#'   "sheets_sheets()", "sheet_names()"
+#' )
+#' knitr::kable(dat, col.names = paste0("**", colnames(dat), "**"))
+#' ```
+#'
+#' @section Range operations:
+#'
+#' ```{r echo = FALSE}
+#' dat <- tibble::tribble(
+#'       ~ "< v0.2.0", ~ ">= v0.2.0",
+#'   "sheets_cells()", "range_read_cells()",
+#'    "sheets_read()", "range_read()"
+#' )
+#' knitr::kable(dat, col.names = paste0("**", colnames(dat), "**"))
+#' ```
+#'
+#' @keywords internal
+#' @name googlesheets4-deprecated
+#' @importFrom lifecycle deprecate_warn
+NULL
+
+# nocov start
+
 #' @rdname googlesheets4-deprecated
 #' @export
-sheets_create <- function(...) {
-  deprecate_warn("0.2.0", "sheets_create()", "gs4_create()")
-  gs4_create(...)
+sheets_sheets <- function(...) {
+  deprecate_warn("0.2.0", "sheets_sheets()", "sheet_names()")
+  sheet_names(...)
 }
 
 #' @rdname googlesheets4-deprecated
 #' @export
-sheets_get <- function(...) {
-  deprecate_warn("0.2.0", "sheets_get()", "gs4_get()")
-  gs4_get(...)
+sheets_cells <- function(...) {
+  deprecate_warn("0.2.0", "sheets_cells()", "range_read_cells()")
+  range_read_cells(...)
+}
+
+#' @rdname googlesheets4-deprecated
+#' @export
+sheets_read <- function(...) {
+  deprecate_warn("0.2.0", "sheets_read()", "range_read()")
+  range_read(...)
 }
 
 #' @rdname googlesheets4-deprecated
@@ -110,24 +118,13 @@ sheets_find <- function(...) {
   gs4_find(...)
 }
 
-#' @section Auth and API endpoints:
-#'
-#' ```{r echo = FALSE}
-#' dat <- tibble::tribble(
-#'                ~ "< v0.2.0", ~ ">= v0.2.0",
-#'             "sheets_auth()",   "gs4_auth()",
-#'   "sheets_auth_configure()",   "gs4_auth_configure()",
-#'          "sheets_api_key()",   "gs4_api_key()",
-#'           "sheets_deauth()",   "gs4_deauth()",
-#'        "sheets_endpoints()",   "gs4_endpoints()",
-#'        "sheets_has_token()",   "gs4_has_token()",
-#'        "sheets_oauth_app()",   "gs4_oauth_app()",
-#'            "sheets_token()",   "gs4_token()",
-#'              "sheet_user()",   "gs4_user()"
-#' )
-#' knitr::kable(dat, col.names = paste0("**", colnames(dat), "**"))
-#' ```
-#'
+#' @rdname googlesheets4-deprecated
+#' @export
+sheets_get <- function(...) {
+  deprecate_warn("0.2.0", "sheets_get()", "gs4_get()")
+  gs4_get(...)
+}
+
 #' @rdname googlesheets4-deprecated
 #' @export
 sheets_auth <- function(...) {

39033c57e4e4e556c3158c110db92cc4e0a28c8b
Jenny Bryan <jenny.f.bryan@gmail.com>
2021-03-05 09:19:41 -0800

    Eliminate unused stop() wrappers

--- R/glue.R
+++ R/glue.R
@@ -12,13 +12,3 @@ stop_glue <- function(..., .sep = "", .envir = parent.frame(),
     call. = call., domain = .domain
   )
 }
-
-stop_glue_data <- function(..., .sep = "", .envir = parent.frame(),
-                           call. = FALSE, .domain = NULL) {
-  stop(
-    glue_data(..., .sep = .sep, .envir = .envir),
-    call. = call., domain = .domain
-  )
-}
-
-stop_collapse <- function(x) stop(glue_collapse(x, sep = "\n"), call. = FALSE)

650c97f292301675fc05c6dd304822b1d1d9791e
Jenny Bryan <jenny.f.bryan@gmail.com>
2021-07-08 15:04:14 -0700

    Move gs4_abort() into utils-ui.R

--- R/utils-ui.R
+++ R/utils-ui.R
@@ -92,3 +92,38 @@ gs4_warning <- function(text, .envir = parent.frame()) {
 gs4_danger <- function(text, .envir = parent.frame()) {
   gs4_alert(text, type = "danger", .envir = .envir)
 }
+
+#' Error conditions for the googlesheets4 package
+#'
+#' @param class Use only if you want to subclass beyond `googlesheets4_error`
+#'
+#' @keywords internal
+#' @name gs4-errors
+#' @noRd
+NULL
+
+gs4_abort <- function(message, ..., class = NULL, .envir = parent.frame()) {
+  g <- function(line) glue(line, .envir = .envir)
+  msg <- map_chr(message, g)
+  abort(msg, class = c(class, "googlesheets4_error"), ...)
+}
+
+sq <- function(x) glue::single_quote(x)
+dq <- function(x) glue::double_quote(x)
+bt <- function(x) glue::backtick(x)
+
+class_collapse <- function(x) {
+  glue("<{glue_collapse(class(x), sep = '/')}>")
+}
+
+# helpful in the default method of an as_{to} generic
+# exists mostly to template the message
+abort_unsupported_conversion <- function(from, to) {
+  if (is.null(from)) {
+    msg_from <- bt("NULL")
+  } else {
+    msg_from <- glue("something of class {class_collapse(from)}")
+  }
+  gs4_abort("
+    Don't know how to make an instance of {class_collapse(to)} from {msg_from}")
+}
--- tests/testthat/test-utils-ui.R
+++ tests/testthat/test-utils-ui.R
@@ -4,3 +4,30 @@ test_that("gs4_quiet() falls back to NA if googlesheets4_quiet is unset", {
     expect_true(is.na(gs4_quiet()))
   )
 })
+
+test_that("gs4_abort() throws classed condition", {
+  expect_error(gs4_abort("oops"), class = "googlesheets4_error")
+  expect_gs4_error(gs4_abort("oops"))
+  expect_gs4_error(gs4_abort("oops", class = "googlesheets4_foo"))
+  expect_error(
+    gs4_abort("oops", class = "googlesheets4_foo"),
+    class = "googlesheets4_foo"
+  )
+})
+
+test_that("gs4_abort() glues data in", {
+  x <- "this"
+  expect_gs4_error(gs4_abort("`x` is {sq(x)}"), "`x` is 'this'", fixed = TRUE)
+
+  # message has length > 1, named bullets
+  expect_snapshot_error(
+    gs4_abort(c("`x` is {sq(x)}", i = "{sq(x)} is in `x`", x = "oops"))
+  )
+})
+
+test_that("abort_unsupported_conversion() works", {
+  x <- structure(1, class = c("a", "b", "c"))
+  expect_snapshot_error(
+    abort_unsupported_conversion(x, "d")
+  )
+})
